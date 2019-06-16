4780a2a1c061aada3d509cd6b0ba86eef4897e6a
Erik Kamalov <ekamalov967@gmail.com>
2019-06-16 15:39:47 +0600

    Initial Commit

---
+++ EKCollectionLayout/EKCollectionLayout.h
@@ -0,0 +1,19 @@
+//
+//  EKCollectionLayout.h
+//  EKCollectionLayout
+//
+//  Created by Erik Kamalov on 6/16/19.
+//  Copyright © 2019 Neuron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EKCollectionLayout.
+FOUNDATION_EXPORT double EKCollectionLayoutVersionNumber;
+
+//! Project version string for EKCollectionLayout.
+FOUNDATION_EXPORT const unsigned char EKCollectionLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EKCollectionLayout/PublicHeader.h>
+
+
