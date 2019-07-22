ebb5290331b56e573204fa3adb6c8bee4e141014
Erik Kamalov <ekamalov967@gmail.com>
2019-07-22 23:01:00 +0600

    added AppStore Layout

---
+++ Source/Layouts/EKAppStoreLayout.swift
@@ -0,0 +1,21 @@
+//
+//  EKAppStoreLayout.swift
+//  EKCollectionLayout
+//
+//  Created by Erik Kamalov on 7/22/19.
+//  Copyright © 2019 Neuron. All rights reserved.
+//
+
+import UIKit
+
+public class EKAppStoreLayout {
+    
+}
+
+
+extension EKAppStoreLayout: LayoutAttributesConfigurator {
+    
+   
+    
+}
+
