22c41cb669b979c0a40ce9bdb2e707740ac57628
Erik Kamalov <ekamalov967@gmail.com>
2019-07-23 11:15:13 +0600

    AppStore Layout complited

--- Source/Layouts/EKAppStoreLayout.swift
+++ Source/Layouts/EKAppStoreLayout.swift
@@ -8,21 +8,34 @@
 
 import UIKit
 
-public class EKAppStoreLayout {
-    init() {
-        
-    }
-}
-
-
-extension EKAppStoreLayout: LayoutAttributesConfigurator {
+public class EKAppStoreLayout:LayoutAttributesConfigurator {
     
     public func prepare(layout flow: EKLayoutFlow) {
-         assert(flow.collectionView.numberOfSections == 1, "Multi section aren't supported!")
-         assert(flow.scrollDirection == .horizontal, "Horizontal scroll direction aren't supported!")
+        assert(flow.collectionView.numberOfSections == 1, "Multi section aren't supported!")
+        assert(flow.scrollDirection == .horizontal, "Horizontal scroll direction aren't supported!")
         
         if flow.collectionView.decelerationRate != .fast  { flow.collectionView.decelerationRate = .fast }
-
+    }
+    public func targetContentOffset(flow: EKLayoutFlow, proposedContentOffset: CGPoint, velocity: CGPoint) -> CGPoint {
+        guard let rectAttributes = flow.layoutAttributesForElements(in: .init(origin: .init(x: proposedContentOffset.x, y: 0), size: flow.collectionView.size)) else { return .zero }
+        var offsetAdjustment = CGFloat.greatestFiniteMagnitude
+        let proposedContentOffsetCenterX = proposedContentOffset.x + flow.collectionView.frame.width / 2
+        
+        for layoutAttributes in rectAttributes {
+            let itemHorizontalCenter = layoutAttributes.center.x
+            if abs(itemHorizontalCenter - proposedContentOffsetCenterX) < abs(offsetAdjustment) {
+                offsetAdjustment = itemHorizontalCenter - proposedContentOffsetCenterX
+            }
+        }
+        var newOffsetX = proposedContentOffset.x + offsetAdjustment
+        let offset = newOffsetX - flow.collectionView.contentOffset.x
+        
+        if (velocity.x < 0 && offset > 0) || (velocity.x > 0 && offset < 0) {
+            let pageWidth = flow.itemSize.width + flow.minimumLineSpacing
+            newOffsetX += velocity.x > 0 ? pageWidth : -pageWidth
+        }
+        
+        return CGPoint(x: newOffsetX, y: proposedContentOffset.y)
     }
     
 }
