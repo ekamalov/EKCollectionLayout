fd7f3bbf2c7ce5e95bf163cdb530e853a1bbdc50
Erik Kamalov <ekamalov967@gmail.com>
2019-07-31 21:07:38 +0600

    added resources example workspace grouped

--- Source/Layouts/EKAppStoreLayout.swift
+++ Source/Layouts/EKAppStoreLayout.swift
@@ -12,30 +12,37 @@ public class EKAppStoreLayout:LayoutAttributesConfigurator {
     
     public func prepare(layout flow: EKLayoutFlow) {
         assert(flow.scrollDirection == .horizontal, "Horizontal scroll direction aren't supported!")
-        
         if flow.collectionView.decelerationRate != .fast  { flow.collectionView.decelerationRate = .fast }
     }
+    
     public func targetContentOffset(flow: EKLayoutFlow, proposedContentOffset: CGPoint, velocity: CGPoint) -> CGPoint {
-        guard let rectAttributes = flow.layoutAttributesForElements(in: .init(origin: .init(x: proposedContentOffset.x, y: 0), size: flow.collectionView.frame.size)) else { return .zero }
-        var offsetAdjustment = CGFloat.greatestFiniteMagnitude
-        let proposedContentOffsetCenterX = proposedContentOffset.x + flow.collectionView.frame.width / 2
-        
-        for layoutAttributes in rectAttributes {
-            let itemHorizontalCenter = layoutAttributes.center.x
-            if abs(itemHorizontalCenter - proposedContentOffsetCenterX) < abs(offsetAdjustment) {
-                offsetAdjustment = itemHorizontalCenter - proposedContentOffsetCenterX
-            }
-        }
-        var newOffsetX = proposedContentOffset.x + offsetAdjustment
-        let offset = newOffsetX - flow.collectionView.contentOffset.x
-        
-        if (velocity.x < 0 && offset > 0) || (velocity.x > 0 && offset < 0) {
-            let pageWidth = flow.itemSize.width + flow.minimumLineSpacing
-            newOffsetX += velocity.x > 0 ? pageWidth : -pageWidth
-        }
-        
-        return CGPoint(x: newOffsetX, y: proposedContentOffset.y)
+        let itemIndex = round(proposedContentOffset.x / flow.itemSize.width)
+        let xOffset = itemIndex * flow.itemSize.width
+        return CGPoint(x: xOffset, y: 0)
     }
+  
+    
+//    public func targetContentOffset(flow: EKLayoutFlow, proposedContentOffset: CGPoint, velocity: CGPoint) -> CGPoint {
+//        guard let rectAttributes = flow.layoutAttributesForElements(in: .init(origin: .init(x: proposedContentOffset.x, y: 0), size: flow.collectionView.frame.size)) else { return .zero }
+//        var offsetAdjustment = CGFloat.greatestFiniteMagnitude
+//        let proposedContentOffsetCenterX = proposedContentOffset.x + flow.collectionView.frame.width / 2
+//
+//        for layoutAttributes in rectAttributes {
+//            let itemHorizontalCenter = layoutAttributes.center.x
+//            if abs(itemHorizontalCenter - proposedContentOffsetCenterX) < abs(offsetAdjustment) {
+//                offsetAdjustment = itemHorizontalCenter - proposedContentOffsetCenterX
+//            }
+//        }
+//        var newOffsetX = proposedContentOffset.x + offsetAdjustment
+//        let offset = newOffsetX - flow.collectionView.contentOffset.x
+//
+//        if (velocity.x < 0 && offset > 0) || (velocity.x > 0 && offset < 0) {
+//            let pageWidth = flow.itemSize.width + flow.minimumLineSpacing
+//            newOffsetX += velocity.x > 0 ? pageWidth : -pageWidth
+//        }
+//
+//        return CGPoint(x: newOffsetX, y: proposedContentOffset.y)
+//    }
     
 }
 
